a95a7ef8e6c753002ae100a00c3c71ef60e74125
sangone88 <llil1@naver.com>
2019-11-18 20:14:41 +0900

    로그인 버튼 관리

--- app/src/main/java/com/tjoeun/a20191118_02_getmethodpractice/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/a20191118_02_getmethodpractice/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.tjoeun.a20191118_02_getmethodpractice
 
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 
 class LoginActivity : BaseActivety() {
@@ -8,9 +10,20 @@ class LoginActivity : BaseActivety() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setupEvents()
+        setValues()
     }
     override fun setupEvents() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        loginBtn.setOnClickListener {
+            if (rememberIdCheckBox.isChecked) {
+                Toast.makeText(mContext, "아이디를 저장해야 합니다.", Toast.LENGTH_SHORT).show()
+
+            }
+            else{
+                Toast.makeText(mContext, "아이디 저장 X" , Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun setValues() {
